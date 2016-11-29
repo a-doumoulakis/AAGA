d80b101b594cd4184d462ae7772b47300b7db456
tanguinoche <tanguy.retail@caramail.com>
2016-11-29 18:17:20 +0100

    added ex 1

---
+++ src/randint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* We use Knuth & Lewis generator (from Seminumerical Algorithms) */
+long m = 4294967296; // 2^32
+long x_n;
+void init_random() {
+  x_n = time(NULL) % m;
+}
+long rand_int(long upper_bound) {
+  int a = 1664525;
+  int c = 1013904223;
+  x_n = (a * x_n + c) % m;
+  return x_n % upper_bound;
+}
+
+
+// int main(void) {
+//   init_random();
+//   for(int i = 0; i < 10; i++) {
+//     printf("%ld \n", rand_int(100000L));
+//   }
+//   return EXIT_SUCCESS;
+// }
---
+++ src/unarybinary.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct ub_node {
+  struct ub_node *child[2];
+};
+typedef struct ub_node UB;
+
+
+
+UB *remyGeneration(int size) {
+  if(size <= 0) return NULL;
+  //Container that provides efficient access to labelled node
+  UB **node = (UB **)malloc(sizeof(UB*) * (size+1));
+  //Keep tracks of parents
+  UB **parent = (UB **)malloc(sizeof(UB*) * (size+1));
+  //We allocate each node
+  int i;
+  for(i=1; i<size+1; i++) {
+    node[i] = (UB *)malloc(sizeof(UB));
+    if(node[i]==NULL) { fprintf(stderr,"allocation error"); exit(EXIT_FAILURE); }
+    node[i]->child[0] = NULL;
+    node[i]->child[1] = NULL;
+    parent[i] = NULL;
+  }
+  //At any time, node[0] contains the root. At the beginning, the root is node[1]
+  node[0] = node[1];
+
+  //We also need to keep the weight of the structure, at the beginning, the weight is 3
+  int tree_weight = 3;
+
+  //Initialization of the integer generator
+  init_random();
+  int k;
+  int j;
+  int weight; //Weight of the node
+  int pos;
+
+  //Algorithm
+  for(i=1; i<size; i++) {
+    //Pick a k integer between 1 and total_weight
+    k = rand_int(tree_weight) +1;
+    //Find the associate node
+    j = 0; //index of the selected node
+
+    while(k>0) {
+      j++;
+      if(node[j]->child[0] != NULL && node[j]->child[0] != NULL) weight = 1;      //Binary node
+      else if(node[j]->child[0] == NULL && node[j]->child[1] == NULL) weight = 3; //Leaf
+      else weight = 2;                                                            //Unary node
+      k -= weight;
+    }
+    switch(weight) {
+      case 1 : //Leaf
+        pos = rand_int(6);
+        if(pos<=1) { //P = 2/6
+          node[j]->child[0] = node[i+1];
+          parent[i+1] = node[j];
+        }
+        else if(pos<=3) { //P = 2/6
+          node[j]->child[1] = node[i+1];
+          parent[i+1] = node[j];
+        }
+        else {
+          (pos==4) ? (node[i+1]->child[0] = node[j]) : (node[i+1]->child[1] = node[j]);
+          //Le parent du noeud j a pour enfant (gauche|droit) le nouveau noeud i+1
+          if(parent[j]!=NULL)(parent[j]->child[0]==node[j]) ? (parent[j]->child[0] = node[i+1]) : (parent[j]->child[1] = node[i+1]);
+          else node[0] = node[i+1]; // The new node is the new root
+          //Le parent de i+1 est le parent de j
+          parent[i+1]=parent[j];
+          //Le nouveau parent de j est le nouveau noeud i+1
+          parent[j] = node[i+1];
+        }
+      break;
+      case 2 : //Unary node
+        //On tire si on insère le nouveau noeud en tant que fils de j, ou bien le noeud j en tant que fils (gauche|droit) du nouveau noeud i+1
+        pos = rand_int(3);
+        if(pos==0) { //P=1/3
+          (node[j]->child[0]==NULL) ? (node[j]->child[0]=node[i+1]) : (node[j]->child[1]=node[i+1]);
+          parent[i+1] = node[j];
+        }
+        else {
+          (pos==1) ? (node[i+1]->child[0] = node[j]) : (node[i+1]->child[1] = node[j]);
+          //Le parent du noeud j a pour enfant (gauche|droit) le nouveau noeud i+1
+          if(parent[j]!=NULL)(parent[j]->child[0]==node[j]) ? (parent[j]->child[0] = node[i+1]) : (parent[j]->child[1] = node[i+1]);
+          else node[0] = node[i+1]; // The new node is the new root
+          //Le parent de i+1 est le parent de j
+          parent[i+1]=parent[j];
+          //Le nouveau parent de j est le nouveau noeud i+1
+          parent[j] = node[i+1];
+        }
+      break;
+      case 3 : //Binary node
+        //On tire si on insère le noeud j en tant que fils (gauche|droit) du nouveau noeud i+1
+        (rand_int(2)==0) ? (node[i+1]->child[0] = node[j]) : (node[i+1]->child[1] = node[j]);
+        //Le parent du noeud j a pour enfant (gauche|droit) le nouveau noeud i+1
+        if(parent[j]!=NULL)(parent[j]->child[0]==node[j]) ? (parent[j]->child[0] = node[i+1]) : (parent[j]->child[1] = node[i+1]);
+        else node[0] = node[i+1]; // The new node is the new root
+        //Le parent de i+1 est le parent de j
+        parent[i+1]=parent[j];
+        //Le nouveau parent de j est le nouveau noeud i+1
+        parent[j] = node[i+1];
+      break;
+    }
+    tree_weight += 2;
+
+  }
+
+  return node[0];
+}
+
+int main(void) {
+
+  return EXIT_SUCCESS;
+}
